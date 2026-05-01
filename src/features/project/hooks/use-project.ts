832da70e22bbc68e0777a2a799e31d94a000c048
HuShaoQiong <1043914966@qq.com>
2026-05-01 21:14:22 +0800

    08: projects

--- src/features/project/hooks/use-project.ts
+++ src/features/project/hooks/use-project.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/purity */
+
 import { useMutation, useQuery } from "convex/react";
 
 import { api } from "../../../../convex/_generated/api";
@@ -11,4 +13,32 @@ export const useProjectsPartial = (limit: number) => {
   return useQuery(api.projects.getPartial, {
     limit,
   });
+};
+
+export const useCreateProject = () => {
+  return useMutation(api.projects.create)
+    // 乐观更新
+    .withOptimisticUpdate(
+      (localStore, args) => {
+        // localStore.getQuery获取当前查询的数据
+        const existingProjects = localStore.getQuery(api.projects.get);
+
+        if (existingProjects !== undefined) {
+          const now = Date.now();
+          const newProject = {
+            _id: crypto.randomUUID() as Id<"projects">,
+            _creationTime: now,
+            name: args.name,
+            ownerId: "anonymous",
+            updatedAt: now,
+          };
+
+          // 类似于react，将数组视为只读
+          localStore.setQuery(api.projects.get, {}, [
+            ...existingProjects,
+            newProject,
+          ]);
+        }
+      }
+    )
 };
